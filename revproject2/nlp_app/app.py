30a2fc0a1cee484a725044680bde7c4bc9a6a5c9
PaulAREnriquez <paulaldrinr.enriquez@gmail.com>
2023-01-27 00:08:09 +0800

    edited views.py

--- revproject2/nlp_app/app.py
+++ revproject2/nlp_app/app.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
             # Text preprocessing
             cleaned_tweet = text_preprocess(payload)
 
-            # Using Our TfidfVectorizer to transform the tokens from our Training Data
+            # Using Our TfidfVectorizer to vectorize the preprocessed text
             tfidf_tweet = NlpAppConfig.tfidf.transform(cleaned_tweet).toarray()
 
             # Converting our vector into tensor
